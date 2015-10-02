9699746597d2868ea71d585c3c56082077848a06
VS <viveks@elayers.net>
2015-10-02 16:43:02 +0530

    Update mysql_line_comment_toggle.py

--- mysql_line_comment_toggle.py
+++ mysql_line_comment_toggle.py
@@ -3,13 +3,17 @@ import sys
 import StringIO
 block = sys.stdin.read()
 block = StringIO.StringIO(block)
+msg = ''
 for line in block:
 	if "-- " in line:
-		line = line.translate(None, '-- ')
+		line = line.replace('-- ','')
+		msg = "All lines in selection uncommented"
 	else:
 		line = "-- " + line
+		msg = "All lines in selection commented"
 	sys.stdout.write(line)
-exit()
+exit(msg)
+
 ## Set gedit external tools params as below:
 ## "Save" ==> "Current Document"
 ## "Input" ==> "Current Selection"
